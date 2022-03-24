1b3d7aedd2249ef253db0df6de4a3ec80609d876
Getulio-Mendes <mendes.getulio@outlook.com>
2022-03-24 16:46:49 -0300

    Support for multiple definitions of a charater

--- dicParse.py
+++ dicParse.py
@@ -1,20 +1,51 @@
 def parseLines():
-    simplified = []
-    definition = []
+    newDic = {}
     for line in dic:
-        definition.append(line.split('/',1)[-1][:-1])
         try:
-            simplified.append(line.split(' ',2)[1])
+            char = line.split(' ',2)[1]
+            defi = line.split('/',1)[-1][:-1]
+        
+            if char in newDic:
+                # hard code limit of different definitions
+                for i in range(6):
+                    if i==0 and char+'1' not in newDic:
+                        newDic[char+'1'] = defi 
+                        break
+
+                    elif char+str(i) in newDic:
+                        newDic[char+str(i+1)] = defi 
+                        break
+            else:
+                newDic[char] = defi
         except:
-            print(line)
+            print(f"Failed to parse: {line}")
+            continue
 
-    return dict(zip(simplified,definition))
+    return newDic
 
-def getDicDef(simplified):
-    if simplified in dic.keys():
-        return dic[simplified]
+def getDicDef(char):
+    question = f"Use definition for {char}:"
+    count = 1
+    answer = dic[char]
+
+    if char in dic.keys():
+        for i in range(6):
+            if i==0 and char in dic:
+                question += f"\n{i}:{dic[char]}\n"
+            elif char+str(i) in dic:
+                question += f"\n{i}:{dic[char+str(i)]}\n"
+                count += 1
+
+        if count != 1:
+            opt = input(question)
+            if opt == '0':
+                answer = dic[char]
+            else:
+                answer = dic[char+str(opt)]
+        return answer
+
     else:
-        return "Failed to get"
+        return f"Failed to get {char}"
 
 with open("./Dictionaries/cedict_ts.u8","r",encoding='utf8') as file:
     dic = file.read()
